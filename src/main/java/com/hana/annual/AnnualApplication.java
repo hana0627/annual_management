8c58433656e9f3e91381f1f1da15fef271a58f6f
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-08-06 17:34:27 +0900

    Initial commit

---
+++ src/main/java/com/hana/annual/AnnualApplication.java
@@ -0,0 +1,13 @@
+package com.hana.annual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnualApplication.class, args);
+	}
+
+}
